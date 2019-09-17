b0a6665f18ca1d0d9daa3fe1e76e247b20589737
FlorentBrandani <florent.brandani@etu.hesge.ch>
2019-09-17 14:38:42 +0200

    Avancée exercices 17.06.2019

--- 11.1- Exercice - Boucle/boucle1.py
+++ 11.1- Exercice - Boucle/boucle1.py
@@ -2,3 +2,11 @@
 # Données : un tableau A de n nombre réels
 # Résultat attendu : Moyenne des nombres réels du tableau A
 A = [1, 5, 15, 25, 10, 55, 50, 35]
+
+moy: float = 0
+i: int = 0
+while i < len(A):
+    moy += + A[i] / len(A)
+    i = i + 1
+
+print(moy)
--- 11.2- Exercice - Boucle/boucle2.py
+++ 11.2- Exercice - Boucle/boucle2.py
@@ -3,3 +3,13 @@
 # Résultat attendu : Le tableau B trié
 
 B = [2, 6, 8, 5, 4, 12, 98, 34, 1]
+
+for j in range(1, len(B)):
+    i = j - 1
+    cle = B[j]
+    while i >= 0 and B[i] > cle:
+        B[i+1] = B[i]
+        i = i-1
+    B[i+1] = cle
+
+print(B)
